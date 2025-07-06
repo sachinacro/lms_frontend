9487cdeee4c313302094f4cbb319f6eee5446a65
sachinacro <sachinsaini08883@gmail.com>
2025-07-06 14:22:30 +0530

    file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
+// vit.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  server: {
+    host: true,
+    port: 3000
+  }
 })
