b0140510f44c9350f7a9e27b51ff01729dbf6365
sachinacro <sachinsaini08883@gmail.com>
2025-07-07 14:14:11 +0530

    file

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { CourseContextProvider } from "./context/CourseContext.jsx";
 
 export const server = import.meta.env.VITE_API_BASE_URL;
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserContextProvider>
